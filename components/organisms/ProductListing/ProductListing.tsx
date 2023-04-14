c984761de4b224ae6d391c3b89e6a4c7c6e56cb2
Dominik Rubröder <dominik.rubroeder@mediawave.de>
2023-04-14 14:01:21 +0200

    Add Sorting actions to Product Listing Component

--- components/organisms/ProductListing/ProductListing.tsx
+++ components/organisms/ProductListing/ProductListing.tsx
@@ -7,45 +7,79 @@ import { useEffect, useState } from "react";
 export default function ProductListing() {
   const animationControls = useAnimationControls();
   const [products, setProducts] = useState([...Array(9).keys()]);
+  const [sortingMode, setSortingMode] = useState<"up" | "down">("up");
   const [listingMode, setListingMode] = useState<"grid" | "row">("grid");
 
   useEffect(() => {
-    animationControls.start({ opacity: [0, 1], y: [100, 0] });
-  }, [listingMode, animationControls]);
+    if (animationControls && listingMode && sortingMode)
+      animationControls.start({ opacity: [0, 1], y: [100, 0] });
+  }, [animationControls, listingMode, sortingMode]);
 
   return (
     <div className="grid gap-8">
-      <div className="flex justify-end gap-4">
-        <div
-          className="interactive flex gap-1 rounded-xl bg-gray-200 p-4"
-          onClick={() => setListingMode("grid")}
-        >
-          <span
-            className={`h-full w-2 rounded-full ${
-              listingMode === "grid" ? "bg-accent" : "bg-gray-300"
-            }`}
-          ></span>
-          <span
-            className={`h-full w-2 rounded-full ${
-              listingMode === "grid" ? "bg-accent" : "bg-gray-300"
-            }`}
-          ></span>
+      <div className="flex justify-end gap-8">
+        <div className="flex items-center gap-4">
+          <div className="flex gap-2">
+            <button
+              className="interactive grid items-center justify-center gap-1 rounded-xl bg-gray-200 p-4"
+              onClick={() => setSortingMode("up")}
+            >
+              <span
+                className={`h-2 w-6 rounded-full ${
+                  sortingMode === "up" ? "bg-accent" : "bg-gray-300"
+                }`}
+              ></span>
+              <span className="h-2 w-6 rounded-full bg-gray-200"></span>
+            </button>
+
+            <button
+              className="interactive grid items-center justify-center gap-1 rounded-xl bg-gray-200 p-4"
+              onClick={() => setSortingMode("down")}
+            >
+              <span className="h-2 w-6 rounded-full bg-gray-200"></span>
+              <span
+                className={`h-2 w-6 rounded-full ${
+                  sortingMode === "down" ? "bg-accent" : "bg-gray-300"
+                }`}
+              ></span>
+            </button>
+          </div>
         </div>
 
-        <div
-          className="interactive grid items-center justify-center gap-1 rounded-xl bg-gray-200 p-4"
-          onClick={() => setListingMode("row")}
-        >
-          <span
-            className={`h-2 w-6 rounded-full ${
-              listingMode === "row" ? "bg-accent" : "bg-gray-300"
-            }`}
-          ></span>
-          <span
-            className={`h-2 w-6 rounded-full ${
-              listingMode === "row" ? "bg-accent" : "bg-gray-300"
-            }`}
-          ></span>
+        <div className="flex items-center gap-4">
+          <div className="flex gap-2">
+            <button
+              className="interactive flex gap-1 rounded-xl bg-gray-200 p-4"
+              onClick={() => setListingMode("grid")}
+            >
+              <span
+                className={`h-full w-2 rounded-full ${
+                  listingMode === "grid" ? "bg-accent" : "bg-gray-300"
+                }`}
+              ></span>
+              <span
+                className={`h-full w-2 rounded-full ${
+                  listingMode === "grid" ? "bg-accent" : "bg-gray-300"
+                }`}
+              ></span>
+            </button>
+
+            <button
+              className="interactive grid items-center justify-center gap-1 rounded-xl bg-gray-200 p-4"
+              onClick={() => setListingMode("row")}
+            >
+              <span
+                className={`h-2 w-6 rounded-full ${
+                  listingMode === "row" ? "bg-accent" : "bg-gray-300"
+                }`}
+              ></span>
+              <span
+                className={`h-2 w-6 rounded-full ${
+                  listingMode === "row" ? "bg-accent" : "bg-gray-300"
+                }`}
+              ></span>
+            </button>
+          </div>
         </div>
       </div>
 
@@ -54,12 +88,13 @@ export default function ProductListing() {
           listingMode === "grid" ? "sm:grid-cols-2 md:grid-cols-3" : ""
         }`}
       >
-        <AnimatePresence>
+        <AnimatePresence initial={false}>
           {products.map((_, index) => (
             <motion.li
               key={index}
               animate={animationControls}
               exit={{ opacity: 0 }}
+              transition={{ delay: index * 0.02 }}
             >
               {listingMode === "grid" ? (
                 <ProductBox />
